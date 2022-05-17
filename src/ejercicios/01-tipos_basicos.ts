ec8a0c066a7f7a5563899fefe456ee3779b44a95
dsandelc <diegosanchezd@hotmail.com>
2022-05-17 23:49:17 +0200

    Git inicial ts-intro

---
+++ src/ejercicios/01-tipos_basicos.ts
@@ -0,0 +1,19 @@
+/*
+    ===== Código de TypeScript =====
+*/
+
+let nombre: string = 'Strider';
+const nombre2 = 'Strider';
+
+nombre = 'Diego';
+
+let hp: number | string = 95;
+
+hp = 'Full';
+
+let estaVivo: boolean = false;
+
+console.log(nombre, hp);
+
+
+
